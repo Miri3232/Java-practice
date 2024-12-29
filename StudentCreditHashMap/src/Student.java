2fe240e7770977ac888a03200680b19956d8b23c
Miri3232 <Maljjang4@gmail.com>
2024-12-30 00:48:56 +0900

    학생 학점, 장학금 계산 HashMap

---
+++ StudentCreditHashMap/src/Student.java
@@ -0,0 +1,46 @@
+import java.util.*;
+class Student {
+	String name, major;
+	int number;
+	double credit;
+	
+	public Student(String name, String major, int number, double credit) {
+		this.name = name;
+		this.major = major;
+		this.number = number;
+		this.credit = credit;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setMajor(String major) {
+		this.major = major;
+	}
+	
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	
+	public void setCredit(double credit) {
+		this.credit = credit;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getMajor() {
+		return major;
+	}
+	
+	public int getNumber() {
+		return number;
+	}
+	
+	public double getCredit() {
+		return credit;
+	}
+	
+}
---
+++ StudentCreditHashMap/src/StudentCredit.java
@@ -0,0 +1,95 @@
+import java.util.*;
+public class StudentCredit {
+	Scanner s = new Scanner(System.in);
+	HashMap<String, Student> students = new HashMap<String, Student>();
+	
+	public void read() {
+		System.out.println("4명 이름, 전공, 학번, 학점 입력");
+		for(int i=0; i<4; i++) {
+			System.out.print(">> ");
+			String input = s.nextLine();
+			
+			StringTokenizer st = new StringTokenizer(input, ",");
+			String name = st.nextToken().trim();
+			String major = st.nextToken().trim();
+			int number = Integer.parseInt(st.nextToken().trim());
+			double credit = Double.parseDouble(st.nextToken().trim());
+			
+			Student student = new Student(name, major, number, credit);
+			
+			students.put(name, student);
+		}
+	}
+	
+	public void printAll() {
+		System.out.println("--------------------------------------------------");
+		
+		for(Student student : students.values()) {
+			System.out.print("이름: " + student.getName() + "\t");
+			System.out.print("전공: " + student.getMajor() + "\t");
+			System.out.print("학번: " + student.getNumber() + "\t");
+			System.out.println("학점평균: " + student.getCredit());			
+		}
+		System.out.println("--------------------------------------------------");
+	}
+	
+	public void bestStudent() {
+		List<Student> bestStudents = new ArrayList<>();
+		double bestCredit = 0;
+		
+		for(Student student : students.values()) {
+			if(student.getCredit() > bestCredit) {
+				bestCredit = student.getCredit();
+				bestStudents.clear();
+				bestStudents.add(student);
+			}
+			else if (student.getCredit() == bestCredit) {
+                bestStudents.add(student);
+			}
+		}
+		System.out.print("장학생: ");
+		for (Student student : bestStudents) {
+            System.out.print(student.getName() + " ");		
+		}
+		System.out.println();
+		System.out.println("--------------------------------------------------");
+	}
+	
+	public void print() {
+		while(true) {
+		System.out.print("학생 이름>> ");
+		String name = s.next();
+		
+		if(name.equals("그만"))
+			break;
+		
+		boolean found = false;
+		
+		for(Student student : students.values()) {
+			if(student.getName().equals(name)) {
+				System.out.print("이름: " + student.getName() + "\t");
+				System.out.print("전공: " + student.getMajor() + "\t");
+				System.out.print("학번: " + student.getNumber() + "\t");
+				System.out.println("학점평균: " + student.getCredit());
+				found = true;
+				break;
+			}
+		}
+			if(!found) {
+				System.out.println(name + " 학생이 없습니다.");
+				}
+			}
+		}
+	
+	public void run() {
+		read();
+		printAll();
+		bestStudent();
+		print();
+	}
+	
+	public static void main(String[] args) {
+		StudentCredit sc = new StudentCredit();
+		sc.run();
+	}
+}
